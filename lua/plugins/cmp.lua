1eb276de1c99f6bd0a4ed7210d73b95d0a5190b6
N3gD0r <70918021+N3gD0r@users.noreply.github.com>
2026-01-11 17:28:42 -0600

    Changed formatting style to lua_ls instead of stylua

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,21 +1,21 @@
 return {
-  {
-    "hrsh7th/nvim-cmp",
-    enabled = true,
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      local cmp = require("cmp")
-      local cmp_select = { behavior = cmp.SelectBehavior.Select }
-      cmp.setup({
-        {
-          mapping = cmp.mapping.preset.insert({
-            ["<Tab>"] = cmp.mapping.confirm({ select = true }),
-            ["<C-n"] = cmp.mapping.select_next_item(cmp_select),
-            ["<C-p"] = cmp.mapping.select_prev_item(cmp_select),
-            ["<C-Space"] = cmp.mapping.complete(),
-          }),
-        },
-      })
-    end,
-  },
+    {
+        "hrsh7th/nvim-cmp",
+        enabled = true,
+        ---@param opts cmp.ConfigSchema
+        opts = function(_, opts)
+            local cmp = require("cmp")
+            local cmp_select = { behavior = cmp.SelectBehavior.Select }
+            cmp.setup({
+                {
+                    mapping = cmp.mapping.preset.insert({
+                        ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+                        ["<C-n"] = cmp.mapping.select_next_item(cmp_select),
+                        ["<C-p"] = cmp.mapping.select_prev_item(cmp_select),
+                        ["<C-Space"] = cmp.mapping.complete(),
+                    }),
+                },
+            })
+        end,
+    },
 }
--- lua/plugins/csharp.lua
+++ lua/plugins/csharp.lua
@@ -1,27 +1,27 @@
 return {
-  {
-    "seblyng/roslyn.nvim",
-    ---@module 'roslyn.config'
-    ---@type RoslynNvimConfig
-    opts = {
-      -- your configuration comes here; leave empty for default settings
+    {
+        "seblyng/roslyn.nvim",
+        ---@module 'roslyn.config'
+        ---@type RoslynNvimConfig
+        opts = {
+            -- your configuration comes here; leave empty for default settings
+        },
+        -- config = function()
+        --   vim.lsp.config("roslyn", {
+        --     settings = {
+        --       ["csharp|inlay_hints"] = {
+        --         csharp_enable_inlay_hints_for_implicit_object_creation = true,
+        --         chsarp_enable_inhay_hints_for_implicit_variable_types = true,
+        --       },
+        --       ["csharp|code_lens"] = {
+        --         dotnet_enable_references_code_lens = true,
+        --       },
+        --       ["csharp|background_analysis"] = {
+        --         dotnet_analyzer_diagnostics_scope = "fullSolution",
+        --         dotnet_compiler_diagnostics_scope = "fullSolution",
+        --       },
+        --     },
+        --   })
+        -- end,
     },
-    -- config = function()
-    --   vim.lsp.config("roslyn", {
-    --     settings = {
-    --       ["csharp|inlay_hints"] = {
-    --         csharp_enable_inlay_hints_for_implicit_object_creation = true,
-    --         chsarp_enable_inhay_hints_for_implicit_variable_types = true,
-    --       },
-    --       ["csharp|code_lens"] = {
-    --         dotnet_enable_references_code_lens = true,
-    --       },
-    --       ["csharp|background_analysis"] = {
-    --         dotnet_analyzer_diagnostics_scope = "fullSolution",
-    --         dotnet_compiler_diagnostics_scope = "fullSolution",
-    --       },
-    --     },
-    --   })
-    -- end,
-  },
 }
